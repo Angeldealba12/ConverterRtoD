fae878c4814341614e2dc58e82bccd1d3da1a165
Angel <angel.dealba.garcia@gmail.com>
2021-07-18 19:06:04 -0500

    converter from radians to degrees

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState(0)
+  const [value1, setValue1] = useState(0)
+
+  const toRadian = (grades) => {
+    return grades * 0.01745;
+  };
+  const toDegrees= (radians) => {
+    return radians * 57.296;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Degrees</h3>
+        <input value={value} onChange={e => setValue(e.target.value)}/>
+        <h3>radians</h3>
+        <input value={value1} onChange={e => setValue1(e.target.value)}/>
+        <button onClick={() => {
+            setValue1(toRadian(value))
+            setValue(toDegrees(value1))
+            }}>Convert</button>
       </header>
     </div>
   );
